5ffdfc7e343ef40a6f33ebaa437b0a5ff18c38c3
caohe <576038240@qq.com>
2018-01-23 12:21:38 +0800

    添加html

---
+++ static/js/0.4dcdc695d66b3144efa9.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],{"3nMw":function(e,t){},mzkE:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("div")},staticRenderFns:[]};var a=n("X4nt")({name:"footerHtml",data:function(){return{msg:"这是个footer"}}},r,!1,function(e){n("3nMw")},"data-v-72d24640",null);t.default=a.exports}});
+//# sourceMappingURL=0.4dcdc695d66b3144efa9.js.map
